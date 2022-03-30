02826fc4d0a172c6dd70fe7ca059bc690d5346fc
Federica Ventura <Venturafederica87@hotmail.it>
2022-03-30 17:26:00 +0200

    Fix conflicts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FirstSectionComponent } from './homepage/sections/first-section/first-section.component';
 import { BunnyComponent } from './homepage/animations/bunny/bunny.component';
+import { FooterContainerComponent } from './footer/footer-container/footer-container.component';
+import { FooterLinkComponent } from './footer/footer-link/footer-link.component';
+import { FooterLogoComponent } from './footer/footer-logo/footer-logo.component';
+import { FooterSocialComponent } from './footer/footer-social/footer-social.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     FirstSectionComponent,
-    BunnyComponent
+    BunnyComponent,
+    FooterContainerComponent,
+    FooterLinkComponent,
+    FooterLogoComponent,
+    FooterSocialComponent
   ],
   imports: [
     BrowserModule,
