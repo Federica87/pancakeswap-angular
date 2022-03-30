336ac89d087894b6c5b45e2d9ee8c7b7a056a550
Federica Ventura <Venturafederica87@hotmail.it>
2022-03-30 17:39:12 +0200

    fix conflicts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,28 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-<<<<<<< HEAD
 import { FirstSectionComponent } from './homepage/sections/first-section/first-section.component';
 import { BunnyComponent } from './homepage/animations/bunny/bunny.component';
-=======
->>>>>>> feature/footer
 import { FooterContainerComponent } from './footer/footer-container/footer-container.component';
 import { FooterLinkComponent } from './footer/footer-link/footer-link.component';
 import { FooterLogoComponent } from './footer/footer-logo/footer-logo.component';
 import { FooterSocialComponent } from './footer/footer-social/footer-social.component';
-<<<<<<< HEAD
 
-=======
->>>>>>> feature/footer
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
     FirstSectionComponent,
     BunnyComponent,
-=======
->>>>>>> feature/footer
     FooterContainerComponent,
     FooterLinkComponent,
     FooterLogoComponent,
