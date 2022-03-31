cc3bac0ac1f27ab1f172aeaefb0b0d565a573ff2
Claudio Atzori <claudio.atzori02@gmail.com>
2022-03-31 12:41:18 +0200

    Fast refactoring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { FirstSectionComponent } from './homepage/sections/first-section/first-section.component';
-import { BunnyComponent } from './homepage/animations/bunny/bunny.component';
-import { FooterContainerComponent } from './footer/footer-container/footer-container.component';
-import { FooterLinkComponent } from './footer/footer-link/footer-link.component';
-import { FooterLogoComponent } from './footer/footer-logo/footer-logo.component';
-import { FooterSocialComponent } from './footer/footer-social/footer-social.component';
-import { NavbarContainerComponent } from './navbar/navbar-container/navbar-container.component';
-import { NavbarLeftComponent } from './navbar/navbar-left/navbar-left.component';
-import { NavbarRightComponent } from './navbar/navbar-right/navbar-right.component';
-import { SecondSectionComponent } from './homepage/sections/second-section/second-section.component';
+import { FirstSectionComponent } from './components/homepage/sections/first-section/first-section.component';
+import { BunnyComponent } from './components/homepage/animations/bunny/bunny.component';
+import { FooterContainerComponent } from './components/footer/footer-container/footer-container.component';
+import { FooterLinkComponent } from './components/footer/footer-link/footer-link.component';
+import { FooterLogoComponent } from './components/footer/footer-logo/footer-logo.component';
+import { FooterSocialComponent } from './components/footer/footer-social/footer-social.component';
+import { NavbarContainerComponent } from './components/navbar/navbar-container/navbar-container.component';
+import { NavbarLeftComponent } from './components/navbar/navbar-left/navbar-left.component';
+import { NavbarRightComponent } from './components/navbar/navbar-right/navbar-right.component';
+import { SecondSectionComponent } from './components/homepage/sections/second-section/second-section.component';
 
 
 @NgModule({
