05064a622d192308fcbb7b79aef7d518ae1c8269
Sharon Mae <duransharonmae@gmail.com>
2022-04-08 18:57:43 +0200

    fix navbar

--- src/app/components/navbar/navbar-left/navbar-left.component.ts
+++ src/app/components/navbar/navbar-left/navbar-left.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { earn, nft, other, trade, win } from 'src/app/models/INav';
 
 @Component({
   selector: 'app-navbar-left',
@@ -6,6 +7,69 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar-left.component.css']
 })
 export class NavbarLeftComponent implements OnInit {
+
+  trades: trade[] = [
+    {
+      trade: "Exchange"
+    },
+    {
+      trade: "Liquidity"
+    },
+  ]
+
+  earns: earn[] = [
+    {
+      earn: "Farms"
+    },
+    {
+      earn: "Pools"
+    }
+  ]
+
+  wins: win[] = [
+    {
+      win: "TRading Competitions"
+    },
+    {
+      win: "Prediction (BETA)"
+    },
+    {
+      win: "Lottery"
+    }
+  ]
+
+  nfts: nft[] = [
+    {
+      nft: "Overview"
+    },
+    {
+      nft: "Collections"
+    },
+    {
+      nft: "Activity"
+    }
+  ]
+
+  others: other[] = [
+    {
+      other: "Info"
+    },
+    {
+      other: "IFO"
+    },
+    {
+      other: "Voting"
+    },
+    {
+      other: "Leaderboard"
+    },
+    {
+      other: "Blog"
+    },
+    {
+      other: "Docs"
+    }
+  ]
 
   constructor() { }
 
---
+++ src/app/models/INav.ts
@@ -0,0 +1,19 @@
+export interface trade {
+    trade: string;
+}
+
+export interface earn {
+    earn: string;
+}
+
+export interface win {
+    win: string;
+}
+
+export interface nft {
+    nft: string;
+}
+
+export interface other {
+    other: string;
+}
