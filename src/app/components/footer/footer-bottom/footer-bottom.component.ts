e9c468e1a1e4214414720d0738a27658701bd1db
Federica Ventura <Venturafederica87@hotmail.it>
2022-04-19 18:02:44 +0200

    Languages

--- src/app/components/footer/footer-bottom/footer-bottom.component.ts
+++ src/app/components/footer/footer-bottom/footer-bottom.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterBottomComponent implements OnInit {
 
+  languages: Array<string> = ['العربية', 'বাংলা', 'English', 'Deutsch', 'Ελληνικά', 'Español', 'Suomalainen', 'Filipino', 'Français', 'हिंदी', 'Magyar', 'Bahasa Indonesia', 'Italiano', '日本語', '한국어', 'Nederlands', 'Polski', 'Português (Brazil)', 'Português', 'Română', 'Русский', 'Svenska', 'தமிழ்', 'Türkçe', 'Українська', 'Tiếng Việt', '简体中文', '繁體中文']
+
   constructor() { }
 
   ngOnInit(): void {
