c1d514e0144ffde4865f9bca2963bd9866c88455
Francesco Girgenti <girgenti65@gmail.com>
2022-04-20 16:37:53 +0200

    statsMockup + efficiency fixes

--- src/app/components/homepage/data/cake-stats/cake-stats.component.ts
+++ src/app/components/homepage/data/cake-stats/cake-stats.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { cakeStatsMockup } from 'src/app/mockup-data/statsData';
 import { Stats } from 'src/app/models/Stats';
 
 @Component({
@@ -8,30 +9,13 @@ import { Stats } from 'src/app/models/Stats';
 })
 export class CakeStatsComponent implements OnInit {
 
-  cakeStats: Stats [] = [
-    {
-      name: "Total supply",
-      data: 265869181
-    },
-    {
-      name: "Burned to date",
-      data: 413201331
-    },
-    {
-      name: "Market cap",
-      data: 2.4
-    },
-    {
-      name: "Current emissions",
-      data: 14.25
-    }
-  ]
+  cakeStats: Stats [] = cakeStatsMockup
 
-  cakeStats2: number [] = [265869181, 413201331];
-
-  constructor() { 
-    setInterval(() => this.cakeStats2[0] += Math.floor(Math.random() * (100 - 10) + 10), 5000)
-    setInterval(() => this.cakeStats2[1] += 1, 10000)
+  constructor() {
+    setInterval(() => {
+      this.cakeStats[0].data -= 1;
+      this.cakeStats[1].data += 1;
+    },30)
   }
 
   ngOnInit(): void {
---
+++ src/app/mockup-data/statsData.ts
@@ -0,0 +1,76 @@
+import { Stats } from "../models/Stats";
+
+export const farmStatsMockup: Stats[] = [
+    {
+      name: "TEM-BUSD LP",
+      data:  0,
+      info: "APR"
+    },
+    {
+      name: "YEL-BNB LP",
+      data: 0,
+      info: "APR"
+    },
+    {
+      name: "GMI-BNB LP",
+      data: 0,
+      info: "APR"
+    },
+    {
+      name: "PEX-BNB LP",
+      data: 0,
+      info: "APR"
+    },
+    {
+      name: "HOTCROSS-BNB LP",
+      data: 0,
+      info: "APR"
+    }
+  ]
+
+export const SyrupStatsMockup: Stats[] = [
+    {
+      name: "Stake CAKE - Earn XWG",
+      data: 0,
+      info: "APR"
+    },
+    {
+      name: "Stake CAKE - Earn GMI",
+      data: 0,
+      info: "APR"
+    },
+    {
+      name: "Stake CAKE - Earn PEX",
+      data: 0,
+      info: "APR"
+    },
+    {
+      name: "Stake CAKE - Earn ERA",
+      data: 0,
+      info: "APR"
+    },
+    {
+      name: "Stake CAKE - Earn DUET",
+      data: 0,
+      info: "APR"
+    }
+  ]
+
+export const cakeStatsMockup: Stats[] = [
+    {
+      name: "Total supply",
+      data: 265869181
+    },
+    {
+      name: "Burned to date",
+      data: 413201331
+    },
+    {
+      name: "Market cap",
+      data: 2.4
+    },
+    {
+      name: "Current emissions",
+      data: 14.25
+    }
+  ]
