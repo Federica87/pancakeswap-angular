2037ef2801627ff48b22f8b879e76c536b78bda4
Federica Ventura <Venturafederica87@hotmail.it>
2022-05-02 15:20:00 +0200

    Fix routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule,Routes } from '@angular/router';
-import { EarnComponent } from './components/earn/earn.component';
 import { FarmsContainerComponent } from './components/earn/farms/farms-container/farms-container.component';
 import { PoolsContainerComponent } from './components/earn/pools/pools-container/pools-container.component';
 import { HomepageContainerComponent } from './components/homepage/homepage-container/homepage-container';
@@ -9,12 +8,9 @@ import { LimitContainerComponent } from './components/trade/limit/limit-containe
 const routes: Routes = [
     {path: '', redirectTo: 'home', pathMatch: 'full'},
     {path: 'home', component: HomepageContainerComponent},
-    {path: 'earn', component: EarnComponent,
-        children:[
-            {path:'farms', component:FarmsContainerComponent},
-            {path:'pools', component: PoolsContainerComponent},
-        ]},
-    { path: 'limit', component: LimitContainerComponent },
+    {path:'farms', component:FarmsContainerComponent},
+    {path:'pools', component: PoolsContainerComponent},
+    {path: 'limit', component: LimitContainerComponent },
     {path: '**', redirectTo: 'home'}
 
 ]
