53649aad95eaa63b7843d10020bfe23bbe418fba
Claudio Atzori <claudio.atzori02@gmail.com>
2022-05-03 17:23:28 +0200

    Pools-card creata

--- src/app/components/earn/earn-routing.module.ts
+++ src/app/components/earn/earn-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { EarnComponent } from "./earn.component";
 import { FarmsContainerComponent } from "./farms/farms-container/farms-container.component";
+import { PoolsContainerComponent } from "./pools/pools-container/pools-container.component";
 
 const routes: Routes= [
     {path:'earn',
      component:EarnComponent,
      children: [
-        {path:'farms', component:FarmsContainerComponent}
+        {path:'farms', component:FarmsContainerComponent},
+        {path:'pools', component:PoolsContainerComponent}
      ]
     },
 ]
