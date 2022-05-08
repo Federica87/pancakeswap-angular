ce8ceeebef1e67e76855048d9632087ae2e0387d
Federica Ventura <Venturafederica87@hotmail.it>
2022-05-09 00:07:46 +0200

    Fix Pools

--- src/app/mockup/poolsData.ts
+++ src/app/mockup/poolsData.ts
@@ -3,48 +3,56 @@ import { Pools } from "../models/Pools";
 export const poolsCrypto: Pools[] = [
     {
         crypto: 'RPG.svg',
+        name: 'RPG',
         total: 1072546,
         apr: 58.22,
         end: 719019
     },
     {
         crypto: 'ANKR.svg',
+        name: 'ANKR',
         total: 908928,
         apr: 44.50,
         end: 1382665
     },
     {
         crypto: 'CEEK.svg',
+        name: 'CEEK',
         total: 741355,
         apr: 52.70,
         end: 289944
     },
     {
         crypto: 'TINC.svg',
+        name: 'TINC',
         total: 1349645,
         apr: 61.19,
         end: 88824
     },
     {
         crypto: 'FROYO.svg',
+        name: 'FROYO',
         total: 410104,
         apr: 54.91,
         end: 581508
     },
     {
         crypto: 'BSW.svg',
+        name: 'BSW',
         total: 3373072,
         apr: 54.83,
         end: 267003
     },
     {
         crypto: 'DUET.svg',
+        name: 'DUET',
         total: 166749,
         apr: 45.54,
         end: 179776
     },
     {
         crypto: 'GMT.svg',
+        name: 'GMT',
         total: 33306513,
         apr: 63.69,
         end: 125116
--- src/app/models/Pools.ts
+++ src/app/models/Pools.ts
@@ -1,5 +1,6 @@
 export interface Pools {
     crypto: string,
+    name: string,
     total: number,
     apr: number,
     end: number
