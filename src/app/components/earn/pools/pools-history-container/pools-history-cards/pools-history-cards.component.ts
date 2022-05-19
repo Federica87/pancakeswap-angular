f787bb0bccdc58023845f1590e4f14663c1eaa0d
Ventura Federica <Venturafederica87@hotmail.it>
2022-05-19 18:23:36 +0200

    Finished Pools Cards

--- src/app/components/earn/pools/pools-history-container/pools-history-cards/pools-history-cards.component.ts
+++ src/app/components/earn/pools/pools-history-container/pools-history-cards/pools-history-cards.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { finishedPools } from './../../../../../mockup/poolsData';
+import { Pools } from 'src/app/models/Pools';
+import { Component, OnInit, Input } from '@angular/core';
+import { WalletService } from 'src/app/services/wallet.service';
 
 @Component({
   selector: 'app-pools-history-cards',
@@ -7,7 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PoolsHistoryCardsComponent implements OnInit {
 
-  constructor() { }
+  @Input() pools: Pools[] = finishedPools;
+
+  details: boolean[] = Array.from({length: this.pools.length}).map(x => x = false);
+
+  openDetails(index: number): void {
+    if (this.details[index] === true) {
+      this.details[index] = false;
+    }
+    else {
+      this.details[index] = true;
+    }
+    console.log(index)
+  }
+
+  connectWallet(): void {
+    this.walletService.connect();
+  }
+
+  constructor(private walletService: WalletService) { }
 
   ngOnInit(): void {
   }
