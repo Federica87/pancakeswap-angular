d35adcac46bcd27fa7add13061a8d88e14c6ec5f
Claudio Atzori <claudio.atzori02@gmail.com>
2022-05-20 00:29:35 +0200

    Responsive tabs pools

--- src/app/components/earn/pools/pools-table/pools-table.component.ts
+++ src/app/components/earn/pools/pools-table/pools-table.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { poolsCrypto } from 'src/app/mockup/poolsData';
 import { Pools } from 'src/app/models/Pools';
+import { WalletService } from 'src/app/services/wallet.service';
 
 @Component({
   selector: 'app-pools-table',
@@ -9,6 +10,23 @@ import { Pools } from 'src/app/models/Pools';
 })
 export class PoolsTableComponent implements OnInit {
   @Input() pools: Pools[] = [];
+
+  details: boolean[] = Array.from({length: this.pools.length}).map(x => x = false);
+
+  openDetails(index: number): void {
+    if (this.details[index] === true) {
+      this.details[index] = false;
+    }
+    else {
+      this.details[index] = true;
+    }
+    console.log(index)
+  }
+  connectWallet(): void {
+    this.walletService.connect();
+  }
+
+  constructor(private walletService: WalletService) { }
 
   ngOnInit(): void {
   }
